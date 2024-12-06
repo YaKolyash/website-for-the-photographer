5eedf60c89ca6ff803fafb08e24bdd3020d9ec3e
Nikolay <kolyashadrin1@gmail.com>
2024-12-07 00:05:47 +0300

    add class item-red

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('mousedown', function(e) {
     if (e.target.matches('.item-link')) {
         e.target.classList.toggle('item-link');
-        e.target.classList.toggle('red');
+        e.target.classList.toggle('item-red');
     }
 });
 
 document.addEventListener('mouseup', function(e) {
-    if (e.target.matches('.red')) {
+    if (e.target.matches('.item-red')) {
         e.target.classList.toggle('item-link');
-        e.target.classList.toggle('red');
+        e.target.classList.toggle('item-red');
     }
 });
