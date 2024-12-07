bca5858cc3a7e31fd268abf7b59da7a4af7642e6
Nikolay <kolyashadrin1@gmail.com>
2024-12-07 21:21:04 +0300

    add burger menu

--- js/script.js
+++ js/script.js
@@ -11,3 +11,40 @@ document.addEventListener('mouseup', function(e) {
         e.target.classList.toggle('item-red');
     }
 });
+
+// ---------------BURGER----------------------------------------------------
+
+const hamb = document.querySelector("#navbar__hamb-field");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#nav").cloneNode(1);
+const body = document.body;
+
+menu.classList.remove(".nav");
+menu.classList.add(".popup__menu");
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+};
+
+function renderPopup() {
+  popup.appendChild(menu);
+};
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+};
+
